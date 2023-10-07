0bc1eb020f7a90c85cceb274b7a4ecc378d64211
inmessionante123 <dhruva.sankhe28@gmail.com>
2023-10-06 19:02:22 -0700

    third commit

--- lab0/proc_count.c
+++ lab0/proc_count.c
@@ -4,29 +4,35 @@
 #include <linux/seq_file.h>
 #include <linux/sched.h>
 
-static struct proc_dir_entry *entry;
 
-static int proc_count(struct seq_file *m, void *v){
-	// TODO: it's all yours
+static struct proc_dir_entry* my_new_entry;
+
+static int seq_show(struct seq_file *s, void *v) {
+	int processCount = 0;
+	struct task_struct* t;
+	for_each_process(t) {
+		processCount++;
+	}
+	seq_printf(s, "%d\n", processCount);
 	return 0;
 }
 
 static int __init proc_count_init(void)
 {
-	entry = proc_create_single("count", 0, NULL, proc_count);
 	pr_info("proc_count: init\n");
+	my_new_entry = proc_create_single("count", 0644, NULL, seq_show);
 	return 0;
 }
 
 static void __exit proc_count_exit(void)
 {
-	proc_remove(entry);
 	pr_info("proc_count: exit\n");
+	proc_remove(my_new_entry);
 }
 
 module_init(proc_count_init);
 module_exit(proc_count_exit);
 
-MODULE_AUTHOR("Your Name");
-MODULE_DESCRIPTION("CS111 lab0 count proc number");
+MODULE_AUTHOR("Your name");
+MODULE_DESCRIPTION("One sentence description");
 MODULE_LICENSE("GPL");
