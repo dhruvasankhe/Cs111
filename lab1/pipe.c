6975892c43a469b91b9d64a88021850f74907937
inmessionante123 <dhruva.sankhe28@gmail.com>
2023-10-22 19:43:27 -0700

    second iteration

--- lab1/pipe.c
+++ lab1/pipe.c
@@ -6,49 +6,77 @@
 
 int main(int argc, char *argv[])
 {
-	if (argc < 2) {
+	// Checking Arguments
+    if (argc < 2) {
         errno = EINVAL;
-        perror("Error");
-        exit(errno);
+        perror("Usage: command1, command2");
+        exit(EXIT_FAILURE);
     }
 
-    int fd[2];
-    pid_t pid;
+    // Creating Pipes
+    int pipes[argc - 1][2]; // Adjust the size of the pipes array
 
-    // Iterate through the arguments (processes to run)
-    for (int i = 1; i < argc; i++) {
-        if (i != argc - 1 && pipe(fd) < 0) {
-            perror("Pipe creation error");
-            exit(errno);
+    // Create pipes
+    for (int i = 0; i < argc - 2; i++) { // You still create argc-2 pipes
+        if (pipe(pipes[i]) == -1) {
+            perror("pipe");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    for (int i = 0; i < argc - 1; i++) { // Adjust your loop to also fork for the last command
+        pid_t p = fork();
+
+        // Forking Failure Check
+        if (p == -1) {
+            perror("fork");
+            exit(EXIT_FAILURE);
         }
 
-        if ((pid = fork()) < 0) {
-            perror("Fork error");
-            exit(errno);
-        }
-
-        if (pid == 0) { // child
-            if (i != argc - 1) {
-                dup2(fd[1], STDOUT_FILENO);  // Redirect stdout to pipe's write
-                close(fd[0]);
-                close(fd[1]);
+        // Child Process
+        if (p == 0) {
+            // If it's not the first command, redirect standard input from the previous pipe's read end
+            if (i != 0) {
+                dup2(pipes[i - 1][0], STDIN_FILENO);
+                close(pipes[i - 1][1]);
             }
-            execlp(argv[i], argv[i], (char *)NULL);
-            perror("Exec error");
-            exit(errno);
-        } else { // parent
-            if (i != argc - 1) {
-                dup2(fd[0], STDIN_FILENO);   // Redirect stdin to pipe's read
-                close(fd[0]);
-                close(fd[1]);
+
+            // If it's not the last command, redirect standard output to the next pipe's write end
+            if (i < argc - 2) {
+                dup2(pipes[i][1], STDOUT_FILENO);
             }
+
+            // Close all pipes in the child, we've already duplicated the ones we needed
+            for (int j = 0; j < argc - 2; j++) {
+                close(pipes[j][0]);
+                close(pipes[j][1]);
+            }
+
+            // Execute the command
+            execlp(argv[i + 1], argv[i + 1], (char *)NULL);
+            // If execlp returns, it's an error
+            perror("execlp");
+            exit(EXIT_FAILURE);
         }
     }
 
-    // parent waits for all children to finish
-    for (int i = 1; i < argc; i++) {
-        wait(NULL);
+    // In the parent: close all pipes, they're not needed anymore
+    for (int i = 0; i < argc - 2; i++) {
+        close(pipes[i][0]);
+        close(pipes[i][1]);
     }
-	
+
+    // Wait for all child processes to finish
+    for (int i = 0; i < argc - 1; i++) {
+        int status;
+        if (wait(&status) == -1) {
+            perror("wait");
+            exit(EXIT_FAILURE);
+        }
+        if (!WIFEXITED(status) || WEXITSTATUS(status) != 0) {
+            fprintf(stderr, "Error: Child process %d exited with error code %d.\n", i, WEXITSTATUS(status));
+            exit(EXIT_FAILURE);
+        }
+    }
 	return 0;
 }
